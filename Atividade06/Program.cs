b59f9e0c1aa20343702db06cc0f3e6b6095979df
yuri <yuripolicantedealmeida@gmail.com>
2025-04-03 11:26:29 -0300

    Atividade06

---
+++ Atividade06/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a nota do aluno: ");
+        double nota = Convert.ToDouble(Console.ReadLine());
+
+        if (nota >= 9) Console.WriteLine("Conceito: A");
+        else if (nota >= 7) Console.WriteLine("Conceito: B");
+        else if (nota >= 5) Console.WriteLine("Conceito: C");
+        else Console.WriteLine("Conceito: D");
+    }
+}
