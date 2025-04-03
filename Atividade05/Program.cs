274fcd72c5e2ef5bc83b6a79239132f066e89fee
yuri <yuripolicantedealmeida@gmail.com>
2025-04-03 11:24:32 -0300

    Atividade05

---
+++ Atividade05/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0) Console.WriteLine("O número é positivo.");
+        else if (numero < 0) Console.WriteLine("O número é negativo.");
+        else Console.WriteLine("O número é zero.");
+    }
+}
