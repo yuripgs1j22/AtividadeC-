4b5a681d2dbe1dc1500de2a3c65eea5fa4ab2627
yuri <yuripolicantedealmeida@gmail.com>
2025-04-03 11:19:24 -0300

    atividade 01

---
+++ Atividade001/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
