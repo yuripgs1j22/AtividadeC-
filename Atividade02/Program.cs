d818fad9ae54373ecfe395504bd0453bf294f81f
yuri <yuripolicantedealmeida@gmail.com>
2025-04-03 11:20:35 -0300

    Atividade02

---
+++ Atividade02/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+}
