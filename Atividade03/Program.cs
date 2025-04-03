888528e8d5da150d929403ff4a7ed8c92e7e811d
yuri <yuripolicantedealmeida@gmail.com>
2025-04-03 11:21:47 -0300

    Atividade03

---
+++ Atividade03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de 1 a 7: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero == 1) Console.WriteLine("Domingo");
+        else if (numero == 2) Console.WriteLine("Segunda-feira");
+        else if (numero == 3) Console.WriteLine("Terça-feira");
+        else if (numero == 4) Console.WriteLine("Quarta-feira");
+        else if (numero == 5) Console.WriteLine("Quinta-feira");
+        else if (numero == 6) Console.WriteLine("Sexta-feira");
+        else if (numero == 7) Console.WriteLine("Sábado");
+        else Console.WriteLine("Número inválido.");
+    }
+}
